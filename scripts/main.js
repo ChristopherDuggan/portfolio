7d48a292cbc6de218a77d6478da35817a6606d2f
Christopher Duggan <christopherduggan@gmail.com>
2019-07-08 17:05:24 -0400

    implement color change for card borders

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
-const viewChecker = () => {
-
   let logo = document.getElementsByClassName('logo')[0]
+  let card = document.getElementsByClassName('card-border')
+
+const changeColor = () => {
+
 
   let windowHeight = window.innerHeight
 
@@ -19,30 +21,48 @@ const viewChecker = () => {
 
   if (about < windowHeight/2) {
     logo.classList = 'logo red';
+    for (let i = 0; i < card.length; i++) {
+      card[i].style.borderColor="rgba(255, 0, 0, 0.8)";
+    }
   }
 
   if (experience < windowHeight/2) {
     logo.classList = 'logo orange';
+    for (let i = 0; i < card.length; i++) {
+      card[i].style.borderColor="rgba(255, 165, 0, 0.8)";
+    }
   }
 
   if (projects < windowHeight/2) {
     logo.classList = 'logo yellow';
+    for (let i = 0; i < card.length; i++) {
+      card[i].style.borderColor="rgba(255, 255, 0, 0.8)";
+    }
   }
 
   if (skills < windowHeight/2) {
     logo.classList = 'logo green';
+    for (let i = 0; i < card.length; i++) {
+      card[i].style.borderColor="rgba(0, 128, 0, 0.8)";
+    }
   }
 
   if (education < windowHeight/2) {
     logo.classList = 'logo blue';
+    for (let i = 0; i < card.length; i++) {
+      card[i].style.borderColor="rgba(0, 0, 255, 0.8)";
+    }
   }
 
   if (contact < windowHeight/2) {
     logo.classList = 'logo purple';
+    for (let i = 0; i < card.length; i++) {
+      card[i].style.borderColor="rgba(128, 0, 128, 0.8)";
+    }
   }
 };
 
-window.addEventListener('resize', viewChecker)
-window.addEventListener('scroll', viewChecker)
+window.addEventListener('resize', changeColor)
+window.addEventListener('scroll', changeColor)
 
 
