2fb2fb744e5c5013a1d9a490b5400c6c4a50b8c5
Christopher Duggan <christopherduggan@gmail.com>
2019-07-08 17:59:15 -0400

    add highlighting and color transition to nav

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
-  let logo = document.getElementsByClassName('logo')[0]
-  let card = document.getElementsByClassName('card-border')
+  let logo = document.getElementsByClassName('logo')[0];
+  let card = document.getElementsByClassName('card-border');
+  let nav = document.getElementsByClassName('nav-li');
 
 const changeColor = () => {
 
@@ -19,43 +20,75 @@ const changeColor = () => {
   const contact = document.getElementById('contact')
     .getBoundingClientRect().top;
 
-  if (about < windowHeight/2) {
+  if (experience > windowHeight/2) {
     logo.classList = 'logo red';
+    for (let i = 0; i < nav.length; i++) {
+      nav[i].classList.remove('nav-highlight');
+      if (nav[i].classList.contains('red')) {
+        nav[i].classList.add('nav-highlight');
+      }
+    }
     for (let i = 0; i < card.length; i++) {
       card[i].style.borderColor="rgba(255, 0, 0, 0.8)";
     }
-  }
-
-  if (experience < windowHeight/2) {
+  } else if (projects > windowHeight/2) {
     logo.classList = 'logo orange';
+    for (let i = 0; i < nav.length; i++) {
+      nav[i].classList.remove('nav-highlight');
+      if (nav[i].classList.contains('orange')) {
+        nav[i].classList.add('nav-highlight');
+      }
+    }
     for (let i = 0; i < card.length; i++) {
       card[i].style.borderColor="rgba(255, 165, 0, 0.8)";
     }
-  }
-
-  if (projects < windowHeight/2) {
+  } else if (skills > windowHeight/2) {
     logo.classList = 'logo yellow';
+    for (let i = 0; i < nav.length; i++) {
+      nav[i].classList.remove('nav-highlight');
+      if (nav[i].classList.contains('yellow')) {
+        nav[i].classList.add('nav-highlight');
+      }
+    }
     for (let i = 0; i < card.length; i++) {
       card[i].style.borderColor="rgba(255, 255, 0, 0.8)";
     }
   }
 
-  if (skills < windowHeight/2) {
+  else if (education > windowHeight/2) {
     logo.classList = 'logo green';
+    for (let i = 0; i < nav.length; i++) {
+      nav[i].classList.remove('nav-highlight');
+      if (nav[i].classList.contains('green')) {
+        nav[i].classList.add('nav-highlight');
+      }
+    }
     for (let i = 0; i < card.length; i++) {
       card[i].style.borderColor="rgba(0, 128, 0, 0.8)";
     }
   }
 
-  if (education < windowHeight/2) {
+  else if (contact > windowHeight/2) {
     logo.classList = 'logo blue';
+    for (let i = 0; i < nav.length; i++) {
+      nav[i].classList.remove('nav-highlight');
+      if (nav[i].classList.contains('blue')) {
+        nav[i].classList.add('nav-highlight');
+      }
+    }
     for (let i = 0; i < card.length; i++) {
       card[i].style.borderColor="rgba(0, 0, 255, 0.8)";
     }
   }
 
-  if (contact < windowHeight/2) {
+  else {
     logo.classList = 'logo purple';
+    for (let i = 0; i < nav.length; i++) {
+      nav[i].classList.remove('nav-highlight');
+      if (nav[i].classList.contains('purple')) {
+        nav[i].classList.add('nav-highlight');
+      }
+    }
     for (let i = 0; i < card.length; i++) {
       card[i].style.borderColor="rgba(128, 0, 128, 0.8)";
     }
